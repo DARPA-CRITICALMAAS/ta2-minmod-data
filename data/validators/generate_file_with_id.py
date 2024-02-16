08d92d0d7eb22a2d55718c3b08e424ac8615f312
namrata1012 <nsharma4@usc.edu>
2024-02-15 22:40:28 -0800

    Remove newline

--- data/validators/generate_file_with_id.py
+++ data/validators/generate_file_with_id.py
@@ -760,15 +760,6 @@ filename = sys.argv[1]
 new_json_folder = sys.argv[2]
 file_name_without_path = os.path.basename(filename)
 
-
-with open(filename) as file:
-    json_data = json.load(file)
-
-json_string = json.dumps(json_data)
-json_string = remove_non_printable_chars(json_string)
-
-json_data = json.loads(json_string)
-
 file_path = filename
 
 print(filename, new_json_folder)
@@ -788,6 +779,10 @@ if is_json_file_under_data(file_path):
         raise
 
 
+    json_string = json.dumps(json_data)
+    json_string = remove_non_printable_chars(json_string)
+
+    json_data = json.loads(json_string)
     if 'MineralSite' in json_data:
         json_data = add_id_to_mineral_site(json_data, new_json_folder, file_name_without_path)
     elif 'MineralSystem' in json_data:
