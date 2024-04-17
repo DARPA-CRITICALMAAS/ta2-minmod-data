8da86d8dbea22e68e4b27c748126b6de3bdeb665
namrata1012 <nsharma4@usc.edu>
2024-04-16 21:36:46 -0700

    Point to local

--- containers/generate_ta2_outputs.py
+++ containers/generate_ta2_outputs.py
@@ -98,8 +98,9 @@ def main(args):
       }
     }
     ''' % (commodity)
-    print(query_resp_df)
+
     query_resp_df = run_minmod_query(query, values=True)
+    print(query_resp_df)
     if not query_resp_df.empty:
         gt_data_df = pd.DataFrame([
             {
