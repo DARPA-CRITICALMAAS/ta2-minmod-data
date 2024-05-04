56c0b6fe36b5897178609b7a92adc1a8199442c3
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-05-04 15:03:53 -0500

    update

--- containers/generate_ta2_outputs.py
+++ containers/generate_ta2_outputs.py
@@ -32,165 +32,162 @@ def main(args):
     # Map group IDs to the dataframe
     df_all_sites['group_id'] = df_all_sites['ms1.value'].map(group_mapping)
 
-    df_all_sites.to_csv('./all_sites.csv')
-    print(df_all_sites)
-
     # ------------------ Mineral Site to Grade-Tonnage data  ------------------
 
     # get aggregated (and transformed) grade-tonnage data for Mineral Sites
-    # query = '''
-    # SELECT 
-    #   ?ms                                              # Mineral Site URI
-    #   ?ms_name                                         # Mineral Site Name
-    #   ?country                                         # Country
-    #   ?loc_wkt                                         # WKT Geometry
-    #   ?state_or_province
-    #   ?total_tonnage_measured
-    #   ?total_tonnage_indicated
-    #   ?total_tonnage_inferred
-    #   ?total_contained_measured
-    #   ?total_contained_indicated
-    #   ?total_contained_inferred
-    #   (?total_tonnage_measured + ?total_tonnage_indicated + ?total_tonnage_inferred AS ?total_tonnage)                     # Total Tonnage [million tonnes]
-    #   (?total_contained_measured + ?total_contained_indicated + ?total_contained_inferred AS ?total_contained_metal)
-    #   (IF(?total_tonnage > 0, ?total_contained_metal / ?total_tonnage, 0) AS ?total_grade)                                 # Total Grade
-    # WHERE {
-    #   {
-    #     SELECT ?ms ?ms_name ?country ?loc_wkt ?state_or_province
-    #            (SUM(?tonnage_measured) AS ?total_tonnage_measured)
-    #            (SUM(?tonnage_indicated) AS ?total_tonnage_indicated)
-    #            (SUM(?tonnage_inferred) AS ?total_tonnage_inferred)
-    #            (SUM(?contained_measured) AS ?total_contained_measured)
-    #            (SUM(?contained_indicated) AS ?total_contained_indicated)
-    #            (SUM(?contained_inferred) AS ?total_contained_inferred)
-    #     WHERE {
+    query = '''
+    SELECT 
+      ?ms                                              # Mineral Site URI
+      ?ms_name                                         # Mineral Site Name
+      ?country                                         # Country
+      ?loc_wkt                                         # WKT Geometry
+      ?state_or_province
+      ?total_tonnage_measured
+      ?total_tonnage_indicated
+      ?total_tonnage_inferred
+      ?total_contained_measured
+      ?total_contained_indicated
+      ?total_contained_inferred
+      (?total_tonnage_measured + ?total_tonnage_indicated + ?total_tonnage_inferred AS ?total_tonnage)                     # Total Tonnage [million tonnes]
+      (?total_contained_measured + ?total_contained_indicated + ?total_contained_inferred AS ?total_contained_metal)
+      (IF(?total_tonnage > 0, ?total_contained_metal / ?total_tonnage, 0) AS ?total_grade)                                 # Total Grade
+    WHERE {
+      {
+        SELECT ?ms ?ms_name ?country ?loc_wkt ?state_or_province
+               (SUM(?tonnage_measured) AS ?total_tonnage_measured)
+               (SUM(?tonnage_indicated) AS ?total_tonnage_indicated)
+               (SUM(?tonnage_inferred) AS ?total_tonnage_inferred)
+               (SUM(?contained_measured) AS ?total_contained_measured)
+               (SUM(?contained_indicated) AS ?total_contained_indicated)
+               (SUM(?contained_inferred) AS ?total_contained_inferred)
+        WHERE {
 
-    #         ?ms :mineral_inventory ?mi .
-    #         OPTIONAL { ?ms rdfs:label|:name ?ms_name . FILTER (STR(?ms_name) != "") }
+            ?ms :mineral_inventory ?mi .
+            OPTIONAL { ?ms rdfs:label|:name ?ms_name . FILTER (STR(?ms_name) != "") }
 
-    #         OPTIONAL { ?ms :location_info ?loc .
+            OPTIONAL { ?ms :location_info ?loc .
             
-    #         OPTIONAL { ?loc :country ?country . FILTER (STR(?country) != "") }
-    #         OPTIONAL { ?loc :state_or_province ?state_or_province . FILTER (STR(?state_or_province) != "") }
-    #         OPTIONAL { ?loc :location ?loc_wkt . FILTER (STR(?loc_wkt) != "") }
+            OPTIONAL { ?loc :country ?country . FILTER (STR(?country) != "") }
+            OPTIONAL { ?loc :state_or_province ?state_or_province . FILTER (STR(?state_or_province) != "") }
+            OPTIONAL { ?loc :location ?loc_wkt . FILTER (STR(?loc_wkt) != "") }
             
-    #         }
+            }
             
 
-    #         ?mi :category ?mi_cat .
-    #         ?mi :commodity [ :name ?name ] .
-    #         FILTER(LCASE(STR(?name)) = "%s")
+            ?mi :category ?mi_cat .
+            ?mi :commodity [ :name ?name ] .
+            FILTER(LCASE(STR(?name)) = "%s")
 
-    #         {
-    #             SELECT ?mi (MAX(?ore_val) AS ?max_ore_val) (SAMPLE(?grade_val) AS ?matched_grade_val)
-    #             WHERE {
-    #                 ?mi :ore [ :ore_value ?ore_val_raw; :ore_unit ?ore_unit] .
-    #                 OPTIONAL { ?mi :grade [ :grade_value ?grade_val; :grade_unit ?grade_unit] . }
-    #                 BIND(IF(bound(?ore_val_raw), ?ore_val_raw, 0) AS ?ore_val_pre)
-    #                 BIND(IF(?ore_unit = <https://minmod.isi.edu/resource/Q202>, ?ore_val_pre, IF(?ore_unit = <https://minmod.isi.edu/resource/Q200>, ?ore_val_pre / 1e6, ?ore_val_pre)) AS ?ore_val)
-    #             }
-    #             GROUP BY ?mi
-    #         }
+            {
+                SELECT ?mi (MAX(?ore_val) AS ?max_ore_val) (SAMPLE(?grade_val) AS ?matched_grade_val)
+                WHERE {
+                    ?mi :ore [ :ore_value ?ore_val_raw; :ore_unit ?ore_unit] .
+                    OPTIONAL { ?mi :grade [ :grade_value ?grade_val; :grade_unit ?grade_unit] . }
+                    BIND(IF(bound(?ore_val_raw), ?ore_val_raw, 0) AS ?ore_val_pre)
+                    BIND(IF(?ore_unit = <https://minmod.isi.edu/resource/Q202>, ?ore_val_pre, IF(?ore_unit = <https://minmod.isi.edu/resource/Q200>, ?ore_val_pre / 1e6, ?ore_val_pre)) AS ?ore_val)
+                }
+                GROUP BY ?mi
+            }
 
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "measured"), ?max_ore_val, 0) AS ?tonnage_measured)
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "indicated"), ?max_ore_val, 0) AS ?tonnage_indicated)
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "inferred"), ?max_ore_val, 0) AS ?tonnage_inferred)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "measured"), ?max_ore_val, 0) AS ?tonnage_measured)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "indicated"), ?max_ore_val, 0) AS ?tonnage_indicated)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "inferred"), ?max_ore_val, 0) AS ?tonnage_inferred)
 
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "measured") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_measured)
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "indicated") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_indicated)
-    #         BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "inferred") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_inferred)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "measured") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_measured)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "indicated") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_indicated)
+            BIND(IF(CONTAINS(LCASE(STR(?mi_cat)), "inferred") && ?matched_grade_val > 0, ?max_ore_val * ?matched_grade_val, 0) AS ?contained_inferred)
 
-    #     }
-    #     GROUP BY ?ms ?ms_name ?country ?loc_wkt ?state_or_province
-    #   }
-    # }
-    # ''' % (commodity)
-    # query_resp_df = run_minmod_query(query, values=True)
-    # if not query_resp_df.empty:
-    #     gt_data_df = pd.DataFrame([
-    #         {
-    #             'ms': row['ms.value'],
-    #             'ms_name': row['ms_name.value'] if len(str(row['ms_name.value'])) > 0 else row['ms.value'].split('/')[-1],
-    #             'country': row.get('country.value', None),
-    #             'state_or_province': row.get('state_or_province.value', None),
-    #             'loc_wkt': row.get('loc_wkt.value', None),
-    #             'tot_tonnage_measured': float(row['total_tonnage_measured.value']),
-    #             'tot_tonnage_indicated': float(row['total_tonnage_indicated.value']),
-    #             'tot_tonnage_inferred': float(row['total_tonnage_inferred.value']),
-    #             'tot_contained_measured': float(row['total_contained_measured.value']),
-    #             'tot_contained_indicated': float(row['total_contained_indicated.value']),
-    #             'tot_contained_inferred': float(row['total_contained_inferred.value']),
-    #             'total_tonnage': float(row['total_tonnage.value']),
-    #             'total_grade': float(row['total_grade.value'])
-    #         }
-    #         for index, row in query_resp_df.iterrows()
-    #     ])
+        }
+        GROUP BY ?ms ?ms_name ?country ?loc_wkt ?state_or_province
+      }
+    }
+    ''' % (commodity)
+    query_resp_df = run_minmod_query(query, values=True)
+    if not query_resp_df.empty:
+        gt_data_df = pd.DataFrame([
+            {
+                'ms': row['ms.value'],
+                'ms_name': row['ms_name.value'] if len(str(row['ms_name.value'])) > 0 else row['ms.value'].split('/')[-1],
+                'country': row.get('country.value', None),
+                'state_or_province': row.get('state_or_province.value', None),
+                'loc_wkt': row.get('loc_wkt.value', None),
+                'tot_tonnage_measured': float(row['total_tonnage_measured.value']),
+                'tot_tonnage_indicated': float(row['total_tonnage_indicated.value']),
+                'tot_tonnage_inferred': float(row['total_tonnage_inferred.value']),
+                'tot_contained_measured': float(row['total_contained_measured.value']),
+                'tot_contained_indicated': float(row['total_contained_indicated.value']),
+                'tot_contained_inferred': float(row['total_contained_inferred.value']),
+                'total_tonnage': float(row['total_tonnage.value']),
+                'total_grade': float(row['total_grade.value'])
+            }
+            for index, row in query_resp_df.iterrows()
+        ])
 
-    #     gt_df_only = gt_data_df.drop_duplicates()
-    #     gt_df_only.reset_index(drop=True, inplace=True)
-    #     gt_df_only.set_index('ms', inplace=True)
-    #     gt_df_only['info_count'] = gt_df_only[['country', 'state_or_province', 'loc_wkt']].apply(lambda x: ((x != '') & (x.notna())).sum(), axis=1)
-    #     gt_df_only = gt_df_only.sort_values(by='info_count', ascending=False)
-    #     gt_df_only = gt_df_only[~gt_df_only.index.duplicated(keep='first')]
-    #     gt_df_only.drop(columns=['info_count'], inplace=True)
-    #     gt_df_only.to_csv(f'{output_directory}/{commodity}_mineral_sites_to_grade_tonnage.csv', index=True, mode='w')
+        gt_df_only = gt_data_df.drop_duplicates()
+        gt_df_only.reset_index(drop=True, inplace=True)
+        gt_df_only.set_index('ms', inplace=True)
+        gt_df_only['info_count'] = gt_df_only[['country', 'state_or_province', 'loc_wkt']].apply(lambda x: ((x != '') & (x.notna())).sum(), axis=1)
+        gt_df_only = gt_df_only.sort_values(by='info_count', ascending=False)
+        gt_df_only = gt_df_only[~gt_df_only.index.duplicated(keep='first')]
+        gt_df_only.drop(columns=['info_count'], inplace=True)
+        gt_df_only.to_csv(f'{output_directory}/{commodity}_mineral_sites_to_grade_tonnage.csv', index=True, mode='w')
 
-    # # ------------------ Mineral Site to Deposit Type classification results ------------------
+    # ------------------ Mineral Site to Deposit Type classification results ------------------
 
-    # query = '''
-    #    SELECT ?ms ?ms_name ?deposit_name ?deposit_source ?deposit_confidence ?deposit_group ?deposit_environment ?country ?loc_wkt ?state_or_province
-    #    WHERE {
+    query = '''
+       SELECT ?ms ?ms_name ?deposit_name ?deposit_source ?deposit_confidence ?deposit_group ?deposit_environment ?country ?loc_wkt ?state_or_province
+       WHERE {
 
-    #        ?ms :deposit_type_candidate ?deposit_candidate_uri .
-    #        ?deposit_candidate_uri :source ?deposit_source .
-    #        ?deposit_candidate_uri :confidence ?deposit_confidence .
-    #        ?deposit_candidate_uri :normalized_uri [
-    #            rdfs:label ?deposit_name ;
-    #            :deposit_group ?deposit_group ;
-    #            :environment ?deposit_environment ] .
+           ?ms :deposit_type_candidate ?deposit_candidate_uri .
+           ?deposit_candidate_uri :source ?deposit_source .
+           ?deposit_candidate_uri :confidence ?deposit_confidence .
+           ?deposit_candidate_uri :normalized_uri [
+               rdfs:label ?deposit_name ;
+               :deposit_group ?deposit_group ;
+               :environment ?deposit_environment ] .
 
-    #        ?ms :mineral_inventory ?mi .
-    #        OPTIONAL { ?ms rdfs:label|:name ?ms_name . }
+           ?ms :mineral_inventory ?mi .
+           OPTIONAL { ?ms rdfs:label|:name ?ms_name . }
 
-    #        OPTIONAL { 
-    #        ?ms :location_info ?loc .
-    #        OPTIONAL { ?loc :country ?country . }
-    #        OPTIONAL { ?loc :state_or_province ?state_or_province . }
-    #        OPTIONAL { ?loc :location ?loc_wkt . }
-    #        }
+           OPTIONAL { 
+           ?ms :location_info ?loc .
+           OPTIONAL { ?loc :country ?country . }
+           OPTIONAL { ?loc :state_or_province ?state_or_province . }
+           OPTIONAL { ?loc :location ?loc_wkt . }
+           }
            
 
-    #        ?mi :commodity [ :name ?name ] .
-    #        FILTER(LCASE(STR(?name)) = "%s")
-    #    }
-    #    ''' % (commodity)
-    # query_resp_df = run_minmod_query(query, values=True)
-    # if not query_resp_df.empty:
-    #     deposits_data = pd.DataFrame([
-    #         {
-    #             'ms': row['ms.value'],
-    #             'ms_name': row['ms_name.value'] if len(str(row['ms_name.value'])) > 0 else row['ms.value'].split('/')[-1],
-    #             'country': row.get('country.value', None),
-    #             'state_or_province': row.get('state_or_province.value', None),
-    #             'deposit_type': row.get('deposit_name.value', None),
-    #             'deposit_group': row.get('deposit_group.value', None),
-    #             'deposit_environment': row.get('deposit_environment.value', None),
-    #             'deposit_classification_confidence': row.get('deposit_confidence.value', None),
-    #             'deposit_classification_source': row.get('deposit_source.value', None),
-    #             'loc_wkt': row.get('loc_wkt.value', None)
-    #         }
-    #         for index, row in query_resp_df.iterrows()
-    #     ])
+           ?mi :commodity [ :name ?name ] .
+           FILTER(LCASE(STR(?name)) = "%s")
+       }
+       ''' % (commodity)
+    query_resp_df = run_minmod_query(query, values=True)
+    if not query_resp_df.empty:
+        deposits_data = pd.DataFrame([
+            {
+                'ms': row['ms.value'],
+                'ms_name': row['ms_name.value'] if len(str(row['ms_name.value'])) > 0 else row['ms.value'].split('/')[-1],
+                'country': row.get('country.value', None),
+                'state_or_province': row.get('state_or_province.value', None),
+                'deposit_type': row.get('deposit_name.value', None),
+                'deposit_group': row.get('deposit_group.value', None),
+                'deposit_environment': row.get('deposit_environment.value', None),
+                'deposit_classification_confidence': row.get('deposit_confidence.value', None),
+                'deposit_classification_source': row.get('deposit_source.value', None),
+                'loc_wkt': row.get('loc_wkt.value', None)
+            }
+            for index, row in query_resp_df.iterrows()
+        ])
 
-    #     deposits_df = deposits_data.drop_duplicates()
-    #     deposits_df.reset_index(drop=True, inplace=True)
-    #     deposits_df.set_index(['ms', 'deposit_type'], inplace=True)
-    #     deposits_df['info_count'] = deposits_df[['country', 'state_or_province', 'loc_wkt']].apply(lambda x: ((x != '') & (x.notna())).sum(), axis=1)
-    #     deposits_df_ordered = deposits_df.sort_values(by='info_count', ascending=False)
-    #     deposits_df_ordered = deposits_df_ordered[~deposits_df_ordered.index.duplicated(keep='first')]
-    #     deposits_df_ordered.drop(columns=['info_count'], inplace=True)
-    #     deposits_df_ordered.reset_index(inplace=True)
-    #     deposits_df_ordered.to_csv(f'{output_directory}/{commodity}_mineral_sites_to_deposit_types.csv', index=False, mode='w')
+        deposits_df = deposits_data.drop_duplicates()
+        deposits_df.reset_index(drop=True, inplace=True)
+        deposits_df.set_index(['ms', 'deposit_type'], inplace=True)
+        deposits_df['info_count'] = deposits_df[['country', 'state_or_province', 'loc_wkt']].apply(lambda x: ((x != '') & (x.notna())).sum(), axis=1)
+        deposits_df_ordered = deposits_df.sort_values(by='info_count', ascending=False)
+        deposits_df_ordered = deposits_df_ordered[~deposits_df_ordered.index.duplicated(keep='first')]
+        deposits_df_ordered.drop(columns=['info_count'], inplace=True)
+        deposits_df_ordered.reset_index(inplace=True)
+        deposits_df_ordered.to_csv(f'{output_directory}/{commodity}_mineral_sites_to_deposit_types.csv', index=False, mode='w')
 
     # ------------------ Hyper Site (aggregated group of sites) to Mineral Site ------------------
 
@@ -245,8 +242,7 @@ def main(args):
         sorted_df_all_sites_all_dep.drop(columns=['info_count'], inplace=True)
         sorted_df_all_sites_all_dep.reset_index(inplace=True)
 
-        sorted_df_all_sites_all_dep.to_csv('./new.csv', index=False, mode='w')
-        # sorted_df_all_sites_all_dep.to_csv(f'{output_directory}/{commodity}_mineral_sites_hypersites.csv', index=False, mode='w')
+        sorted_df_all_sites_all_dep.to_csv(f'{output_directory}/{commodity}_mineral_sites_hypersites.csv', index=False, mode='w')
 
 
 if __name__ == "__main__":
@@ -257,4 +253,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
-
